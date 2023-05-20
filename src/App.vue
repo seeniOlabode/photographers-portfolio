4eac89aefc2111c3a74edfbb7a6d06a836aad3ca
Seeni <olabodeodebunmiseeni@gmail.com>
2023-05-20 23:10:26 +0100

    finish loader with css

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <site-loader />
+</template>
+
+<script>
+import SiteLoader from "./components/SiteLoader.vue";
+
+export default {
+  components: {
+    SiteLoader,
+  },
+};
+</script>
+
+<style>
+#app {
+  background-image: repeating-linear-gradient(
+    to right,
+    rgba(172, 172, 194, 0.2),
+    rgba(172, 172, 194, 0.2) 2px,
+    transparent 2px,
+    transparent
+  );
+  background-size: 84px 84px;
+  background-color: #000000;
+  height: 100vh;
+  width: 100vw;
+  color: white;
+}
+</style>
---
+++ src/components/SiteLoader.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="loader">
+    <h1 class="artist-name">
+      <span class="word" v-for="(word, j) in nameWordsArray" :key="word[j] + j">
+        <span
+          class="size-filler"
+          v-for="letter in word"
+          :key="letter + word[i] + i"
+        >
+          {{ letter }}
+          <span class="effect-wrapper">
+            <span v-for="char in allCharactersArray" :key="char + 'char'">{{
+              char
+            }}</span>
+            <span>{{ letter }}</span>
+          </span>
+        </span>
+      </span>
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      nameWords: ["ALAN", "COSPEN"],
+      allCharacters: "0123456789abcdefghijklmnopqrstuvwxyz",
+    };
+  },
+  computed: {
+    allCharactersArray() {
+      return this.allCharacters.split("");
+    },
+    nameWordsArray() {
+      let array = [];
+      this.nameWords.forEach((word, i) => {
+        let wordArray = word.split("");
+        array.push(wordArray);
+      });
+      return array;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.loader {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+.artist-name {
+  font-family: Helvetica, Arial, sans-serif;
+  margin: 0;
+  text-align: center;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  font-size: 128px;
+  font-weight: 400;
+}
+
+.size-filler {
+  color: rgba(0, 0, 0, 0);
+}
+
+.word {
+  position: relative;
+}
+
+.effect-wrapper {
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  animation: effectWrapper 5s;
+  animation-timing-function: cubic-bezier(0.64, 0.03, 0.37, 0.98);
+  animation-fill-mode: forwards;
+  color: rgba(255, 255, 255, 1);
+}
+
+.size-filler:nth-child(odd) .effect-wrapper {
+  animation-delay: 0.2s;
+}
+
+.size-filler:nth-child(3n + 0) .effect-wrapper {
+  animation-delay: 0.3s;
+}
+
+.size-filler:nth-child(4n + 0) .effect-wrapper {
+  animation-delay: 0.5s;
+}
+
+.size-filler {
+  position: relative;
+  overflow: hidden;
+  display: inline-block;
+}
+
+@keyframes effectWrapper {
+  100% {
+    transform: translateY(-97%);
+  }
+}
+</style>
